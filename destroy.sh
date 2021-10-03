21752c46c58659a7dc0bd75a6797a5dcf6fcab88
mdnurakmal <mdnurakmal@gmail.com>
2021-10-03 16:37:27 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -3,7 +3,7 @@
 cd terraform 
 terraform init
 terraform state pull
-terraform destroy -auto-approve -input=false
+terraform destroy -var region=$1 -auto-approve -input=false
 
 gsutil rm -r gs://roxy-cloud-run-bucket
 
